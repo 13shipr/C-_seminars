a2f799ebff7cf591d3d15787770f3377e28612d2
Konstantin Ulyanov <13shipr@mail.ru>
2022-08-11 18:46:02 +0500

    задачи 6 и 8

---
+++ Seminar_001/Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = n%2;
+if (i == 0)
+{
+    Console.WriteLine($"Число: {n} четное");
+}
+else
+{
+    Console.WriteLine($"Число: {n} нечетное");
+}
---
+++ Seminar_001/Task_8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= n)
+{
+    Console.WriteLine($"-> {n= n - n%2}");
+    i++;
+    n = n-1;
+}
