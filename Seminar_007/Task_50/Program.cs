e2c5f846ce1d3b418d8aff980b7f6dc5cbb989c6
Konstantin Ulyanov <13shipr@mail.ru>
2022-08-29 17:45:54 +0500

    дз к семинару 7

---
+++ Seminar_007/Task_50/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+// Console.WriteLine("Задайте массив");
+Console.Write("Введите номер строки: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[5, 5];
+Random rnd = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = rnd.Next(-10, 10);
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+if (m < matrix.GetLength(0) +1 && n < matrix.GetLength(1) +1)
+{
+    Console.WriteLine($"В этом массиве такой элемент есть - это число: {matrix[m-1,n-1]} ");
+}
+else
+{
+    Console.WriteLine($"Элемента в этом массиве не существует");
+}
---
+++ Seminar_007/Task_52/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Задайте массив");
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+Random rnd = new Random();
+int[] sum = new int[n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = rnd.Next(-10, 10);
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+for (int j = 0; j < n; j++)
+{
+    for (int i = 0; i < m; i++)
+    {
+        sum[j] += matrix[i, j];
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое: ");
+foreach (double elem in sum)
+{
+    Console.Write(Math.Round(elem / m, 1) + "\t");
+}
+Console.WriteLine();
