bdb69c14a2eaee53c107895b4f90e9068915e705
Konstantin Ulyanov <13shipr@mail.ru>
2022-08-30 11:49:08 +0500

    дз к семинару 5

---
+++ Seminar_005/Task_34/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.WriteLine("Задайте массив");
+Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] matrix = new int[size];
+Random rnd = new Random();
+for (int i = 0; i < size; i++)
+{
+    {
+        matrix[i] = rnd.Next(100, 1000);
+        Console.Write('[' + string.Join(", ", matrix[i]) + ']');
+    }
+}
+Console.WriteLine();
+int pos = 0;
+for (int i = 0; i < size; i++)
+    {
+        if (matrix[i] % 2 == 0)
+        {
+            pos++;
+        }
+    }
+    Console.WriteLine($"Количество четных чисел в масиве: {pos}");
---
+++ Seminar_005/Task_36/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+Console.WriteLine("Задайте массив");
+Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] matrix = new int[size];
+Random rnd = new Random();
+Console.WriteLine("Массив: ");
+for (int i = 0; i < size; i++)
+{
+    {
+        matrix[i] = rnd.Next(1, 10);
+        Console.Write('[' + string.Join(", ", matrix[i]) + ']');
+    }
+}
+Console.WriteLine();
+int sum = 0;
+for (int i = 0; i < size; i++)
+    {
+        if (matrix[i] % 2 != 0)
+        {
+            sum = matrix[i] + sum;
+        }
+    }
+    Console.WriteLine($"Сумма чисел нечетных элементов в масиве: {sum}");
